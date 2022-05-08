53792cb192620fb26da54c591b1b6de4724d0924
Mohamed Coulibali <konoufo@hotmail.fr>
2022-05-07 23:24:09 -0400

    skeleton

---
+++ screens/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Button } from "react-native-paper";
+
+const Home : React.FC<{}> = () => {
+    return <View>
+        <Button>Évaluation des étudiants</Button>
+    </View>
+}
+export default Home
